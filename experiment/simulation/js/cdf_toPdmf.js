29ccd5d0c81de1fa3d026ef761cc4f902e112d68
Keag44 <agarwalkeshav4400@gmail.com>
2024-05-24 10:07:26 +0530

    Add 3rd part of Exp 1

---
+++ experiment/simulation/js/cdf_toPdmf.js
@@ -0,0 +1,10 @@
+// fixing the continuous RV to be uniform and discrete RV to have only 5 points with non-zero probability
+
+// on dom load
+document.addEventListener("DOMContentLoaded", function () {
+    reset();
+});
+
+function reset(){
+
+}
